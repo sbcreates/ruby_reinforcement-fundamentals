ec4fcad2f7e3fa657b643ecb9b925eaafd4b2d4a
sbcreates <drawsarahb@gmail.com>
2017-08-17 14:50:35 -0400

    Completed #1 of aug 11 reinforcement exercise

---
+++ aug_11_2017.rb
@@ -0,0 +1,18 @@
+train_log = [
+{train: "72C", frequency_in_minutes: 15, direction: "north"},
+{train: "72D", frequency_in_minutes: 15, direction: "south"},
+{train: "610", frequency_in_minutes: 5, direction: "north"},
+{train: "611", frequency_in_minutes: 5, direction: "south"},
+{train: "80A", frequency_in_minutes: 30, direction: "east"},
+{train: "80B", frequency_in_minutes: 30, direction: "west"},
+{train: "110", frequency_in_minutes: 15, direction: "north"},
+{train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+
+p "*===#1===*"
+train8_direction = train_log[7][:direction]
+p train8_direction
+
+p "*===#2===*"
+train8_direction = train_log[7][:direction]
+p train8_direction
