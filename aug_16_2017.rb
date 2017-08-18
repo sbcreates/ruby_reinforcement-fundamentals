bfce3c39732f75a3df7ceda918de48d0de143b34
sbcreates <drawsarahb@gmail.com>
2017-08-17 23:06:20 -0400

    Completed aug 16 assignment

--- aug_16_2017.rb
+++ aug_16_2017.rb
@@ -1,8 +1,41 @@
+#EXERCISE 1
 emotions = {
-  happy => [1, 2, 3],
-  sad => [1, 2, 3],
-  anxious => [1, 2, 3],
-  scared => [1, 2, 3],
-  angry => [1, 2, 3],
-  excited => [1, 2, 3],
+  :happy => 2,
+  :sad => 1,
+  :angry => 3,
+  :stress => 1
 }
+
+#EXERCISE 2
+class Person
+
+  def initialize(name)
+    @name = name
+    @emotions = {
+      :happy => 2,
+      :sad => 1,
+      :anger => 3,
+      :stress => 1
+    }
+  end
+
+  def emotions
+    @emotions
+  end
+
+end
+
+sarah = Person.new("Sarah")
+p sarah.inspect
+
+#EXERCISE 3
+
+sarah.emotions.each do |emotions, level|
+  if level == 1
+    p "I am feeling a low amount of #{emotions}."
+  elsif level == 2
+    p "I am feeling a medium amount of #{emotions}."
+  elsif level == 3
+    p "I am feeling a high amount of #{emotions}."
+  end
+end
