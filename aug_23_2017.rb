96130504609765299ed9005c3975db347a59e1d7
sbcreates <drawsarahb@gmail.com>
2017-08-24 15:09:25 -0400

    Completed aug 23 exercise

---
+++ aug_23_2017.rb
@@ -0,0 +1,36 @@
+hash = { data:
+
+  {
+    rooms:
+    [
+      { id: 1, room_number: "201", capacity: 50},
+      { id: 2, room_number: "301", capacity: 200 },
+      { id: 3, room_number: "1B", capacity: 100}
+    ],
+
+    events:
+    [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+
+#1 Retrieve the capacity of room 201 and store it in a variable.
+room_201_capacity = hash[:data][:rooms][0][:capacity]
+puts room_201_capacity
+
+
+#2 Find all the events taking place in room 201. Iterate through them and print "ok" if the number of planned attendees will fit in the room.
+hash.each do |room, room_id|
+  if hash[:data][:events][1][:room_id] == 1
+    if hash[:data][:events][1][:attendees] == room_201_capacity
+      puts "OK"
+    else
+      puts "Not ok"
+    end
+  end
+end
