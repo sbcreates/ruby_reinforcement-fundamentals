0130819eb916db85491660c08dd8eb891864c754
sbcreates <drawsarahb@gmail.com>
2017-09-15 17:45:10 -0400

    Completed and tested exercise for sept 15

---
+++ sept_15_2017.rb
@@ -0,0 +1,17 @@
+words = ["Totam", "ut", "odit", "quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+
+words.each do |word|
+  if word == word.downcase && word.length > 4
+    puts "long and lowercase"
+  elsif word == word.downcase
+    puts "lowercase"
+  elsif word.length > 4
+    puts "long"
+  else
+    puts "#{word}"
+  end
+end
+
+puts lowercase
+puts capitalize
